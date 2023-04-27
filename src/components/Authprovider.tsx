b0e0a8cb75cfba65b7e50dc8fd1203eb9224dbc6
Pierrot <pierre.bournet@sncf.fr>
2023-04-27 16:28:52 +0200

    fix post

--- src/components/Authprovider.tsx
+++ src/components/Authprovider.tsx
@@ -1,39 +1,51 @@
-import React, { useState, useEffect } from 'react';
-import AuthContext, { AuthContextData } from '../contexts/AuthContext';
-import { User } from '../types/User'
+import React, { useState, createContext } from 'react';
+import { User } from '../types/User';
+
+export interface AuthContextProps {
+  isLoggedIn: boolean;
+  user: User | null;
+  isAdmin: boolean;
+  setAuthContextData: (user: User, token: string) => void; // Ajout du paramètre token
+}
+
+const AuthContext = createContext<AuthContextProps>({
+  isLoggedIn: false,
+  user: null,
+  isAdmin: false,
+  setAuthContextData: () => {}, // Ajout d'une fonction vide par défaut
+});
 
 interface AuthProviderProps {
   children: React.ReactNode;
 }
 
-const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
-  const [authContextData, _setAuthContextData] = useState<AuthContextData>({
-    isAuthenticated: false,
+export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
+  const [authContextData, _setAuthContextData] = useState<AuthContextProps>({
+    isLoggedIn: false,
     user: null,
     isAdmin: false,
-    setAuthContextData: (isAuthenticated: boolean, isAdmin: boolean, user: User | null) => {
-      _setAuthContextData({ ...authContextData, isAuthenticated, isAdmin, user });
-    },
+    setAuthContextData: () => {}, // Ajout d'une fonction vide par défaut
   });
 
-  useEffect(() => {
-    const storedUser = localStorage.getItem('user');
-    const storedIsAdmin = localStorage.getItem('isAdmin') === 'true';
-
-    if (storedUser) {
-      const user: User = JSON.parse(storedUser);
-      _setAuthContextData((prevState) => ({
-        ...prevState,
-        isAuthenticated: true,
-        user,
-        isAdmin: storedIsAdmin,
-      }));
-    }
-  }, []);
+  const setAuthContextData = (user: User, token: string) => {
+    const isAdmin = user.roles.includes('ROLE_ADMIN');
+    _setAuthContextData((prevState: AuthContextProps) => ({
+      ...prevState,
+      isLoggedIn: true,
+      user: {
+        ...user,
+        token, // Ajout du token dans l'objet user
+      },
+      isAdmin,
+      setAuthContextData, 
+    }));
+  };
 
   return (
-    <AuthContext.Provider value={authContextData}>{children}</AuthContext.Provider>
+    <AuthContext.Provider value={authContextData}>
+      {children}
+    </AuthContext.Provider>
   );
 };
 
-export default AuthProvider;
+export default AuthContext;
