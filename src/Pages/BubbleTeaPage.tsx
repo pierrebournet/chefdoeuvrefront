7cecd7e77d3b7f374f49af8bf00f55f3e1a04697
Pierrot <pierre.bournet@sncf.fr>
2023-05-08 16:12:15 +0200

    fix bug dashboard

--- src/Pages/BubbleTeaPage.tsx
+++ src/Pages/BubbleTeaPage.tsx
@@ -2,24 +2,32 @@ import React, { useEffect, useState } from 'react';
 import { Product } from '../types/Product';
 import { Container, Row } from 'react-bootstrap';
 import { useProductContext } from '../contexts/ProductContext';
-import { categories } from '../types/categories';
 import Header from '../components/Header';
 import HeaderConnect from '../components/HeaderConnect';
 import './BubbleTeaPage.css';
+import { Category } from '../types/categories';
+import { fetchCategories } from '../services/category.service';
 
 const BubbleTeaPage: React.FC = () => {
   const { products } = useProductContext();
   const [filteredProducts, setFilteredProducts] = useState<Product[]>([]);
+  const [categories, setCategories] = useState<Category[]>([]);
   const userIsConnected = false;
 
   useEffect(() => {
+    // replace this get categories with the useCOntext category like in dashboard 
+    const getCategories = async () => {
+      const response = await fetchCategories()
+      setCategories(response)
+    }
+    getCategories()
     const bubbleTeaCategory = categories.find(category => category.name === 'Bubble Tea')?.id;
     
     if (bubbleTeaCategory) {
       const filtered = products.filter(product => Number(product.categoryId) === bubbleTeaCategory);
       setFilteredProducts(filtered);
     }
-  }, [products]);
+  }, [products, categories]);
 
   return (
     <>
